f65bb7909f17d1325374877cef0519f83dc4855e
Katariina Linde <klinde@greeny.cs.tlu.ee>
2015-09-21 15:02:26 +0300

    mysql

--- page/login.php
+++ page/login.php
@@ -1,25 +1,31 @@
 <?php
+    //loome AB ühenduse
+    require_once("../../config.php");
+    $database ="if15_klinde";
+    $mysqli= new mysqli($servername, $username, $password, $database);
     
-    //jutumärkide vahele input elemendi NAME
- 
+    //check connection
+    if($mysqli->connect_error){
+        die("connect error ".mysqli_connect_error());
+    }
+
+    
     //ERRORid
     
     $email_error="";
     $password_error="";
 	$password1_error="";
-	$password2_error="";
 	$firstname_error="";
 	$lastname_error="";
-	$epost_error="";
+	$email2_error="";
 	
     //Muutujad väärtustega
     $email = "";
     $password ="";
     $password1="";
-    $password2="";
     $firstname="";
     $lastname="";
-    $epost="";
+    $email2="";
     
       
     //Kontrolli ainult siis, kui kasutaja vajutab logi sisse nuppu
@@ -44,7 +50,27 @@
             
             if($password_error == "" && $email_error == ""){
                 //erroreid ei olnud
-                echo "Kontrollin ".$email." ".$password;
+                echo "Võib sisse logida! ".$email." ".$password;
+                
+                $hash = hash("sha512", $password);
+                $stmt = $mysqli->prepare("SELECT id, email FROM users WHERE email=? AND password=?");
+                //küsimärkide asendus
+                
+                
+                $stmt->bind_param("ss", $email. $hash);
+                //andmebaasist tulnud muutujad
+                $stmt->bind_result($id_from_db, $email_from_db);
+                $stmt->execute();
+                
+                //teeb päringu ja kui on tõene, st oli see olemas
+                if($stmt->fetch()){
+                //kasutaja ja parool õiged
+                    echo "Kasutaja logis sisse ".$id_from_db;
+                }else{
+                    echo "Wrong credentials!";
+                }
+                    
+                $stmt->close();
             }
             
             
@@ -63,11 +89,11 @@
                 //annan väärtuse
                 $lastname = test_input($_POST["lastname"]);            
             }
-            if(empty($_POST["epost"])){
-                $epost_error="Kohustuslik väli";
+            if(empty($_POST["email2"])){
+                $email2_error="Kohustuslik väli";
             }else{
                 //annan väärtuse
-                $epost = test_input($_POST["epost"]);            
+                $email2 = test_input($_POST["email2"]);            
             }
             if(empty($_POST["password1"])){
                 $password1_error="Ei saa olla tühi";
@@ -83,35 +109,47 @@
             }
                 
             }
-            if(empty($_POST["password2"])){
-                $password2_error="Ei saa olla tühi";
-            }else{
-                
-                //parool ei ole tühi, kontrollime pikkust
-                if(strlen($_POST["password2"]) != strlen($_POST["password1"])){
-                    $password2_error="Vale parool";
-                    
-                }else{
-                //annan väärtuse
-                $password2 = test_input($_POST["password2"]);            
-                }
-                
+      
             }
                         
-            if($firstname_error == "" && $lastname_error == "" && $epost_error == "" && $password1_error == "" && $password2_error == ""){
-                //erroreid ei olnud
-                echo "Kontrollin ".$firstname." ".$lastname." ".$epost." ".$password1." ".$password2;
+            if($firstname_error == "" && $lastname_error == "" && $email2_error == "" && $password1_error == "" ){
+                
+                echo hash ("sha512", $password1);
+                echo "Võib kasutaja luua! Kasutaja nimi on ".$email2." ja parool on ".$password1." Nimi on ".$firstname;
+                
+                 //tekitan parooliräsi
+                $hash=hash("sha512", $password1);
+                
+                //salvestan andmebaasi
+                $stmt = $mysqli->prepare("INSERT INTO users (firstname, lastname, email2, password1) VALUES (?,?,?,?)");
+                
+                //kirjutan välja errori
+                //echo $stmt-> error;
+                //echo $mysqli-> error;
+                
+                //paneme muutujad küsimärkide asemel
+                //ss - string, iga muutuja kohta üks täht 
+                $stmt->bind_param("ssss", $firstname, $lastname, $email1, $hash);
+                
+                
+                
+                //käivitab sisestuse
+                $stmt->execute();
+                $stmt->close();
             }
             
         }
         
-    }
+    
     function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
     }
+    
+    //paneme ühenuduse kinni 
+    $mysqli->close();
        
 ?> 
 
@@ -138,9 +176,8 @@
 		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <input type="text" name="firstname" placeholder="Eesnimi" value="<?php echo $firstname; ?>">* <?php echo $firstname_error;?><br><br>
 		<input type="text" name="lastname" placeholder="Perenimi" value="<?php echo $lastname; ?>">*<?php echo $lastname_error;?><br><br>
-		<input type="email" name="epost" placeholder="E-post" value="<?php echo $epost; ?>">*<?php echo $epost_error;?><br><br>
+		<input type="email" name="email2" placeholder="E-post" value="<?php echo $email2; ?>">*<?php echo $email2_error;?><br><br>
 		<input type="password" name="password1" placeholder="Parool" value="<?php echo $password1; ?>">*<?php echo $password1_error;?><br><br>
-		<input type="password" name="password2" placeholder="Korda parooli" value="<?php echo $password2; ?>">*<?php echo $password2_error;?><br><br>
 		<input type="submit" name="create" value="Create">
 		
 		</form>
