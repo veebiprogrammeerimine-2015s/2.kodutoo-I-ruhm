f912c4270ed709d5d76a03e995d4787e63397f45
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-10-24 13:00:46 +0300

    header

--- home.php
+++ home.php
@@ -6,10 +6,10 @@
 	$page_file_name="home.php";
 ?>
 
-<?php require_once ("../header.php"); ?>
+<?php require_once ("./header.php"); ?>
 
 
 	<h1> Avaleht </h1>
 
 
-<?php require_once ("../footer.php"); ?>
+<?php require_once ("./footer.php"); ?>
