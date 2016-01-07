bf5b5b22fd8b4b8d1f5174edeb70276200704f81
Jaanmk <jaan@araneus.ee>
2016-01-07 03:36:16 +0200

    v1.0
    Data was missing fixed

--- functions.php
+++ functions.php
@@ -1,13 +1,20 @@
 <?php
 // Getting a safe input from the user
-function clean_input($data) {
+function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
 //make life easier with defining __ROOT__
 define('__ROOT__', $_SERVER['DOCUMENT_ROOT']);
 
-echo '__ROOT__';
+//add a config to the mix
+require_once(dirname(__ROOT__).'/config_global.php');
+
+//starting a session
+session_start();
+$connection = new mysqli($servername, $server_username, $server_password, $testdb);
+
  ?>
